821a57e8a235af074ae8c3e04cdea32cd29b2615
ngershman496 <ngershman496@g.rwu.edu>
2021-09-25 14:07:45 -0400

    Added Two methods called print_art and flip_coin

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-# This is a sample Python script.
+from random import randint
+from random import seed
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-# This function collects user input to collect a name, number and a word
-# No error checking is implemented so please use proper input
 def get_info():
     name = input("Enter your name: ")
     print(f'Hello, {name}')
@@ -13,15 +9,90 @@ def get_info():
     word = input("Enter a word: ")
     print(f'The length of \'{word}\' is {word.__len__()}')
 
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def print_art():
+    #   |                   |
+    #   |                   |
+    #   |                   |
+    #   |        <^>        |
+    #   ||===I||(-@-)||I===||
+    #   |        \_/        |
+    #   |                   |
+    #   |                   |
+    #   |                   |
+    #   |                   |
+    #   |                   |\
 
-# Press the green button in the gutter to run the script.
+
+    art =[[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+            ['|','|','|','|','|','|','|','|','|','|','|'],
+            [' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','=',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','=',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','=',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','I',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','(',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ','<','-','\\',' ',' ',' ',' ',' '],
+            [' ',' ',' ','^','@','_',' ',' ',' ',' ',' '],
+            [' ',' ',' ','>','-','/',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',')',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','I',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','=',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','=',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ','=',' ',' ',' ',' ',' ',' '],
+            ['|','|','|','|','|','|','|','|','|','|','|']]
+
+    x = len(art)
+    y = len(art[0])
+
+    for i in range(len(art[0])):
+        for j in range(len(art)):
+            print(art[j][i], end='')
+        print()
+
+def flip_coin(i):
+
+    choice = 2
+    while choice != 1 and choice != 0:
+        choice = int(input("Select '0' or '1': "))
+        if choice != 1 and choice != 0:
+            print("Invalid choice")
+    seed((i+5)*i)
+    flip = randint(0,1)
+
+    print("The coin is " , flip)
+    
+    if flip == int(choice):
+        print("You win!")
+    else:
+        print("You lost :(")
+
 if __name__ == '__main__':
-   # print_hi("Nathan")
-    get_info()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    resp = 1
+    counter = 1
+    while resp == 1:
+        choice = 0
+        while choice != 1 and choice != 2:
+            choice = int(input("Would you like to print art(1) or flip a coin(2): "))
+            if choice != 1 and choice != 2:
+                print("Invalid imput\nPlease chose 1 or 2")
+        if choice == 1:
+            print_art()
+        elif choice == 2:
+            flip_coin(counter)
+        
+        resp = input("Enter 1 to go again: ")
+        if resp == '1':
+            resp = 1
+        else:
+            resp = 0
+        counter+=1
