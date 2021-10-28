c90c52abe37bf0ab639a62f1f503652fa0174dd0
ngershman496 <89923898+ngershman496@users.noreply.github.com>
2021-10-28 12:23:13 -0400

    Threading

--- main.py
+++ main.py
@@ -1,67 +1,32 @@
-import turtle
+import threading
+from queue import Queue
+import time
 
-window = turtle.Screen()
-counter = 1
+print_lock = threading.Lock()
 
-t1 = turtle.Turtle()
-t1.speed(-1)
-t1.width(5)
+def job(worker):
+    time.sleep(3)
+    with print_lock:
+        print(threading.current_thread().name, worker)
 
-def drawBoard():
-    t1.pu()
-    t1.shape('circle')
-    t1.shapesize(1)
-    t1.setheading(0)
-    t1.color('black')
-    currentpos = -170
-    for i in range(10):
-        t1.setpos(-170, currentpos)
-        for j in range(10):
-            t1.stamp()
-            t1.forward(50)
-        currentpos = currentpos + 50
-    t1.shape('turtle')
+def threader():
+    while True:
+        worker = q.get()
+        job(worker)
+        q.task_done()
 
-def up():
-    t1.setheading(90)
-    t1.forward(50)
+q = Queue()
 
-def down():
-    t1.setheading(270)
-    t1.forward(50)
+for x in range(10):
+    t = threading.Thread(target = threader)
+    t.daemon = True
+    t.start()
 
-def left():
-    t1.setheading(180)
-    t1.forward(50)
+start = time.time()
 
-def right():
-    t1.setheading(0)
-    t1.forward(50)
+for worker in range(20):
+    q.put(worker)
 
-def blue():
-    t1.color('blue')
+q.join()
 
-def red():
-    t1.color('red')
-
-def switch():
-    if t1.isdown():
-        t1.pu()
-    else:
-        t1.pd()
-
-def main():
-
-    drawBoard()
-    turtle.listen()
-    turtle.onkey(up, 'w')
-    turtle.onkey(down, 's')
-    turtle.onkey(left, 'a')
-    turtle.onkey(right, 'd')
-    turtle.onkey(blue, 'b')
-    turtle.onkey(red, 'r')
-    turtle.onkey(switch,' ')
-
-    window.mainloop()
-
-main()
+print('Done')
