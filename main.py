b2ab448abf9d2e9c5de5e8b2200763444734f75f
ngershman496 <89923898+ngershman496@users.noreply.github.com>
2021-11-01 16:06:22 -0400

    string3 complete

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
-import threading
-from queue import Queue
-import time
+import sys
 
-print_lock = threading.Lock()
+def split1(str):
+    for i in range(len(str)):
+        if i != len(str)-1:
+            if str[i] != ' ' and str[i+1] != ' ':
+                print(str[i], end='')
+            elif str[i] != ' ' and str[i+1] == ' ':
+                print(str[i], end='\n')
+        else:
+            print(str[i])
 
-def job(worker):
-    time.sleep(3)
-    with print_lock:
-        print(threading.current_thread().name, worker)
-
-def threader():
-    while True:
-        worker = q.get()
-        job(worker)
-        q.task_done()
-
-q = Queue()
-
-for x in range(10):
-    t = threading.Thread(target = threader)
-    t.daemon = True
-    t.start()
-
-start = time.time()
-
-for worker in range(20):
-    q.put(worker)
-
-q.join()
-
-print('Done')
+def split2(str):
+    splstr = str.split()
+    for x in splstr:
+        print(x)
+    
+if __name__ == '__main__':
+    mystring = input("Enter a string: ")
+    print('\nFunction 1:')
+    split1(mystring)
+    print('\nFunction 2:')
+    split2(mystring)
